09a021f7b7caa5f66301b0c0fe2da8cdc39e4898
Tapan48 <20BDS059@iiitdwd.ac.in>
2025-12-27 09:48:18 +0530

    resume update 2

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -48,7 +48,7 @@ export function Hero() {
   const handleViewResume = () => {
     // Replace this URL with your Google Drive shared link
     const driveUrl =
-      "https://drive.google.com/file/d/1xJSo1NFwjNTzQcgzfLTwQjS5NaHDjTCu/view?usp=sharing";
+      "https://drive.google.com/file/d/1QlAsy0VWVaCZ7TBwiRgkh9z8aHUY4lSo/view?usp=sharing";
     window.open(driveUrl, "_blank");
   };
 
